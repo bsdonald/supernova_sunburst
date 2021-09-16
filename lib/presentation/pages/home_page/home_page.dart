d0a74e8cab112014799e934b05958c096fcef923
Braden <bsdonald7@gmail.com>
2021-09-16 14:18:36 -0700

    fixed initialization for firebase on web, set up home page body

--- lib/presentation/pages/home_page/home_page.dart
+++ lib/presentation/pages/home_page/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:supernova_sunburst/presentation/pages/home_page/widgets/home_page_body.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,7 +10,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Hello World!'),
       ),
-      body: Container(),
+      body: HomePageBody(),
     );
   }
 }
---
+++ lib/presentation/pages/home_page/widgets/home_page_body.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePageBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(32),
+      child: Container(
+        color: Colors.red,
+      ),
+    );
+  }
+}
