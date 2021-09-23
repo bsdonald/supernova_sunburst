04678bc0cbd5fc551028f191db7fd4c5c2a6c837
Braden <bsdonald7@gmail.com>
2021-09-23 13:33:44 -0700

    configured injection

--- lib/application/auth/sign_in_form/sign_in_form_bloc.dart
+++ lib/application/auth/sign_in_form/sign_in_form_bloc.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:dartz/dartz.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
 import 'package:supernova_sunburst/domain/auth/auth_failure.dart';
 import 'package:supernova_sunburst/domain/auth/i_auth_facade.dart';
 import 'package:supernova_sunburst/domain/auth/value_objects.dart';
@@ -9,7 +10,7 @@ part 'sign_in_form_event.dart';
 part 'sign_in_form_state.dart';
 part 'sign_in_form_bloc.freezed.dart';
 
-// @injectable
+@injectable
 class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
   final IAuthFacade _authFacade;
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:injectable/injectable.dart';
+import 'package:supernova_sunburst/injection.dart';
 import 'package:supernova_sunburst/presentation/core/app_widget.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  configureInjection(Environment.prod);
   await Firebase.initializeApp();
   runApp(AppWidget());
 }
