67f11481157a28fdabb7a2a58cca227f5207d3f9
Braden <bsdonald7@gmail.com>
2021-09-23 09:48:53 -0700

    created value failures, objects, and validators for email address and password. Create Cache package.

--- lib/domain/auth/user.dart
+++ lib/domain/auth/user.dart
@@ -5,7 +5,6 @@ class User extends Equatable{
     required this.id,
     this.email,
     this.name,
-    this.photo,
   });
 
   /// The current user's email address.
@@ -17,9 +16,6 @@ class User extends Equatable{
   /// The current user's name (display name).
   final String? name;
 
-  /// Url for the current user's photo.
-  final String? photo;
-
   /// Empty user which represents an unauthenticated user.
   static const empty = User(id: '');
 
@@ -30,5 +26,5 @@ class User extends Equatable{
   bool get isNotEmpty => this != User.empty;
 
   @override
-  List<Object?> get props => [email, id, name, photo];
+  List<Object?> get props => [email, id, name];
 }
